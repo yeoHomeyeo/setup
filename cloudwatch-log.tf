780bc87b567b07ffcdce81821ee58fbefc8dc77d
Indigo_cE9 <chrisyeohc@outlook.com>
2025-05-21 18:44:01 +0800

    add cloud watch

---
+++ cloudwatch-log.tf
@@ -0,0 +1,60 @@
+# Create CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "ce-grp-2-vpc_flow_logs" {
+  name              = "/aws/vpc/${aws_vpc.ce-grp-2-vpc.id}/flow-logs"
+  retention_in_days = 30  # Adjust retention as needed (1-3653)
+
+  tags = {
+    Name = "${local.env}-ce-grp-2-vpc-flow-logs"
+  }
+}
+
+# IAM Role for VPC Flow Logs
+resource "aws_iam_role" "ce-grp-2-vpc_flow_log_role" {
+  name = "${local.env}-ce-grp-2-vpc-flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "vpc-flow-logs.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "${local.env}-ce-grp-2-vpc-flow-log-role"
+  }
+}
+
+# IAM Policy for Flow Logs
+resource "aws_iam_role_policy" "ce-grp-2-vpc_flow_log_policy" {
+  name = "${local.env}-ce-grp-2-vpc-flow-log-policy"
+  role = aws_iam_role.ce-grp-2-vpc_flow_log_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      Resource = aws_cloudwatch_log_group.ce-grp-2-vpc_flow_logs.arn
+    }]
+  })
+}
+
+# VPC Flow Logs (CloudWatch Destination)
+resource "aws_flow_log" "ce-grp-2-vpc_flow_log" {
+  log_destination      = aws_cloudwatch_log_group.ce-grp-2-vpc_flow_logs.arn
+  log_destination_type = "cloud-watch-logs"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.ce-grp-2-vpc.id
+  iam_role_arn         = aws_iam_role.ce-grp-2-vpc_flow_log_role.arn
+
+  tags = {
+    Name = "${local.env}-ce-grp-2-vpc-flow-log"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -66,10 +66,3 @@ resource "aws_route_table_association" "ce-grp-2-public_zone2" {
   subnet_id      = aws_subnet.ce-grp-2-public_zone2.id
   route_table_id = aws_route_table.ce-grp-2-public.id
 }
-
-resource "aws_flow_log" "vpc_flow_log" {
-  log_group_name = "/aws/vpc/${aws_vpc.ce-grp-2-vpc.id}"
-  vpc_id         = aws_vpc.ce-grp-2-vpc.id
-  traffic_type   = "ALL"
-  iam_role_arn   = aws_iam_role.vpc_flow_log_role.arn
-}
